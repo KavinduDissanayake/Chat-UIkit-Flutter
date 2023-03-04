be24549be5ca7595d002e8a9f106f8697f7bd261
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2023-03-04 08:39:00 +0530

    promo image added

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     GetMaterialApp getXApp = GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       theme: ThemeData(
       primaryColor: AppColors.primaryColor
